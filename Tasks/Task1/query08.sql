dcd8c2aff829718a017944408921edddfd1bf700
Viachaslau Saroka <v.saroka@godeltech.com>
2020-11-23 18:42:44 +0300

    feat: implement query08

--- Tasks/Task1/query08.sql
+++ Tasks/Task1/query08.sql
@@ -1,7 +1,8 @@
---Query 8
-SELECT TOP 10
-       [BusinessEntityID],
-       [Name],
-       [PurchasingWebServiceURL]
-FROM [Purchasing].[Vendor] 
-;
+SELECT BusinessEntityID,
+		Name,
+		PurchasingWebServiceURL
+FROM Purchasing.Vendor
+WHERE ModifiedDate BETWEEN '01/01/2011' AND '12/31/2011'
+ORDER BY CASE 
+	WHEN PurchasingWebServiceURL IS NULL THEN 0 
+	ELSE 1 END DESC, PurchasingWebServiceURL ASC;
