f34bf6b9a80ad3c6eef1c3c42ac6a86ca48f739f
v.saroka <v.saroka@godeltech.com>
2020-11-23 18:17:13 +0300

    feat: implement query02

--- Tasks/Task1/query02.sql
+++ Tasks/Task1/query02.sql
@@ -1,7 +1,7 @@
---Query 2
-SELECT TOP 10
-       [SalesOrderID],
-       [OrderDate],
-       [TotalDue]
-FROM [Sales].[SalesOrderHeader]
-;
+SELECT SalesOrderID,
+		OrderDate, 
+		TotalDue
+FROM Sales.SalesOrderHeader
+WHERE OrderDate = EOMONTH(OrderDate) 
+		AND YEAR(OrderDate) = 2011 
+		AND TotalDue < 1000;
