37a29336390a65db5c7d461591fa60fbab8f07f4
Viachaslau Saroka <v.saroka@godeltech.com>
2020-11-23 18:30:58 +0300

    feat: implement query05

--- Tasks/Task1/query05.sql
+++ Tasks/Task1/query05.sql
@@ -1,6 +1,5 @@
---Query 5
-SELECT TOP 10
-       [CustomerID],
-       [Freight] AS [Freight]
-FROM [Sales].[SalesOrderHeader]
-;
+SELECT TOP 5 CustomerID, SUM(Freight) AS Freight
+FROM Sales.SalesOrderHeader
+WHERE YEAR(OrderDate) = 2013
+GROUP BY CustomerID
+ORDER BY Freight DESC;
