df2ff3c8ee90bd4ff3abb79c95648c070c766695
Viachaslau Saroka <v.saroka@godeltech.com>
2020-11-23 18:38:34 +0300

    feat: implement query06, but didn't pass test

--- Tasks/Task1/query06.sql
+++ Tasks/Task1/query06.sql
@@ -1,6 +1,5 @@
---Query 6
-SELECT TOP 10
-       [CountryRegionCode],
-       '' AS [Name]
-FROM [Person].[CountryRegion]
-;
+SELECT CustomerID,OrderDate,
+		SalesOrderID,CreditCardID,
+ROW_NUMBER() OVER(PARTITION BY CustomerID ORDER BY CustomerID,SalesOrderID) AS rownum
+FROM Sales.SalesOrderHeader
+WHERE CreditCardID IS NOT NULL;
