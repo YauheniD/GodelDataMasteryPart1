1d293c22ffd4d4b2c3caa0b6d738a3b7e661dfb4
Viachaslau Saroka <v.saroka@godeltech.com>
2020-11-23 18:45:59 +0300

    feat: implement query10

--- Tasks/Task1/query10.sql
+++ Tasks/Task1/query10.sql
@@ -1,7 +1,7 @@
---Query 10
-SELECT TOP 10
-       [SalesOrderID],
-       [OrderDate],
-       '' AS [Day Of Week Name]
-FROM [Sales].[SalesOrderHeader]
-;
+SELECT SalesOrderID,OrderDate,
+	DATENAME(WEEKDAY,OrderDate) AS 'Day of week name'
+FROM Sales.SalesOrderHeader
+WHERE YEAR(OrderDate) = 2011 
+	AND MONTH(OrderDate) = 8 
+	AND DATENAME(WEEKDAY,OrderDate) IN ('Saturday','Sunday')
+ORDER BY OrderDate;
