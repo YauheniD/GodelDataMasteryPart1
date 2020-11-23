a30f7923e495ba46a8a32325f84320911aaa8b3b
Viachaslau Saroka <v.saroka@godeltech.com>
2020-11-23 18:44:34 +0300

    feat: implement query09

--- Tasks/Task1/query09.sql
+++ Tasks/Task1/query09.sql
@@ -1,7 +1,8 @@
---Query 9
-SELECT TOP 10
-       [SalesOrderID],
-       [PurchaseOrderNumber],
-       [OnlineOrderFlag]
-FROM [Sales].[SalesOrderHeader]
-;
+SELECT SalesOrderID, 
+		PurchaseOrderNumber, 
+		OnlineOrderFlag
+FROM Sales.SalesOrderHeader
+WHERE ((PurchaseOrderNumber LIKE 'PO1%9' 
+		AND OnlineOrderFlag = 0) 
+		OR (OnlineOrderFlag = 1)) 
+		AND OrderDate = '05/31/2011';
