1e6e138b722e13d3b1cf454e8a3cfb2600ee3af5
Viachaslau Saroka <v.saroka@godeltech.com>
2020-11-30 14:17:33 +0300

    feat: implement query05

--- Tasks/Task2/query05.sql
+++ Tasks/Task2/query05.sql
@@ -1,13 +1,19 @@
-SELECT HumanResources.Employee.BusinessEntityID, 
-		Person.Person.LastName, 
-		CASE 
-			WHEN Person.PersonPhone.PhoneNumberTypeID = 1 THEN Person.PersonPhone.PhoneNumber
-			WHEN Person.PersonPhone.PhoneNumberTypeID != 1 THEN NULL
-		END AS PhoneNumber
+SELECT HumanResources.Employee.BusinessEntityID,
+       Person.Person.LastName,
+       CASE 
+              WHEN Person.PersonPhone.PhoneNumberTypeID = 1
+                     THEN Person.PersonPhone.PhoneNumber
+              WHEN Person.PersonPhone.PhoneNumberTypeID != 1
+                     THEN NULL
+              END AS PhoneNumber
 FROM HumanResources.Employee
 LEFT JOIN Person.Person
-		ON Person.Person.BusinessEntityID = HumanResources.Employee.BusinessEntityID
+       ON Person.Person.BusinessEntityID = HumanResources.Employee.BusinessEntityID
 LEFT JOIN Person.PersonPhone
-		ON Person.PersonPhone.BusinessEntityID = Person.Person.BusinessEntityID
-WHERE Person.Person.PersonType IN ('EM','SP')
-ORDER BY HumanResources.Employee.BusinessEntityID;
+       ON Person.PersonPhone.BusinessEntityID = Person.Person.BusinessEntityID
+WHERE Person.Person.PersonType IN (
+              'EM',
+              'SP'
+              )
+ORDER BY HumanResources.Employee.BusinessEntityID
+;
