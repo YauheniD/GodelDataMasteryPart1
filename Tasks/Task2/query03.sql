5d22cce6e7a9387a2b1dc9dc6b64f0152ed997f9
Viachaslau Saroka <v.saroka@godeltech.com>
2020-11-30 14:15:04 +0300

    feat: implement query03

--- Tasks/Task2/query03.sql
+++ Tasks/Task2/query03.sql
@@ -1,19 +1,32 @@
 SELECT Sales.Customer.CustomerID,
-	CASE
-	WHEN Person.Person.MiddleName IS NULL AND Person.Person.FirstName IS NOT NULL THEN
-				CONCAT(Person.Person.FirstName,' ',
-				Person.Person.LastName)
-		WHEN Person.Person.MiddleName IS NOT NULL THEN
-				CONCAT(Person.Person.FirstName,' ',
-				Person.Person.MiddleName,' ',
-				Person.Person.LastName)
-		END AS CustomerFullName,
-		SalesOrderID,OrderDate
+       CASE 
+              WHEN Person.Person.MiddleName IS NULL
+                     AND Person.Person.FirstName IS NOT NULL
+                     THEN CONCAT (
+                                   Person.Person.FirstName,
+                                   ' ',
+                                   Person.Person.LastName
+                                   )
+               ELSE
+                          CONCAT (
+                                   Person.Person.FirstName,
+                                   ' ',
+                                   Person.Person.MiddleName,
+                                   ' ',
+                                   Person.Person.LastName
+                                   )
+              END AS CustomerFullName,
+       SalesOrderID,
+       OrderDate
 FROM Sales.SalesOrderHeader
 FULL OUTER JOIN Sales.Customer
-			ON Sales.Customer.CustomerID = Sales.SalesOrderHeader.CustomerID
+       ON Sales.Customer.CustomerID = Sales.SalesOrderHeader.CustomerID
 LEFT JOIN Person.Person
-			ON Person.Person.BusinessEntityID = Sales.Customer.PersonID
-WHERE (SalesOrderID IS NULL OR OrderDate = '01/01/2013') AND Sales.Customer.CustomerID IS NOT NULL
+       ON Person.Person.BusinessEntityID = Sales.Customer.PersonID
+WHERE (
+              SalesOrderID IS NULL
+              OR OrderDate = '01/01/2013'
+              )
+       AND Sales.Customer.CustomerID IS NOT NULL
 ORDER BY CustomerID
-
+;
