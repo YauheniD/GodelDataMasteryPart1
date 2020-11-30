79cfe5fd07e8f78f8bede014c06686510bedb15e
Viachaslau Saroka <v.saroka@godeltech.com>
2020-11-30 14:06:24 +0300

    feat: implement query06

---
+++ Tasks/Task2/query06.sql
@@ -0,0 +1,17 @@
+SELECT CustomerID,
+		Production.Product.Name as BikeName,
+		OrderDate,
+		OnlineOrderFlag
+FROM Sales.SalesOrderHeader
+LEFT JOIN Sales.SalesOrderDetail 
+		ON Sales.SalesOrderHeader.SalesOrderID = Sales.SalesOrderDetail.SalesOrderID
+LEFT JOIN Production.Product
+		ON Sales.SalesOrderDetail.ProductID = Production.Product.ProductID
+LEFT JOIN Production.ProductSubcategory
+		ON Production.Product.ProductSubcategoryID = Production.ProductSubcategory.ProductSubcategoryID
+WHERE OrderDate = '01/01/2013' 
+		AND OnlineOrderFlag = 1 
+		AND Production.ProductSubcategory.ProductCategoryID = 1
+ORDER BY CustomerID
+;
+
