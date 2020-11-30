c066a705471d48a64a826973cf0422f7fc06c149
Viachaslau Saroka <v.saroka@godeltech.com>
2020-11-30 14:13:15 +0300

    feat: implement query02

--- Tasks/Task2/query02.sql
+++ Tasks/Task2/query02.sql
@@ -1,11 +1,12 @@
 SELECT TOP 37 Person.BusinessEntityID,
-		Person.FirstName,
-		Person.LastName,
-		Address.City
+       Person.FirstName,
+       Person.LastName,
+       Address.City
 FROM Person.Person AS Person
 INNER JOIN Person.BusinessEntityAddress AS BusinessEntityAddress
-		ON Person.BusinessEntityID = BusinessEntityAddress.BusinessEntityID
-		INNER JOIN Person.Address AS Address
-				ON BusinessEntityAddress.AddressID = Address.AddressID
+       ON Person.BusinessEntityID = BusinessEntityAddress.BusinessEntityID
+INNER JOIN Person.Address AS Address
+       ON BusinessEntityAddress.AddressID = Address.AddressID
 WHERE Address.City = 'Bellevue'
 ORDER BY Person.BusinessEntityID
+;
