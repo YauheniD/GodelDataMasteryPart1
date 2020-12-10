5802bbdc7b59ae0b45fb566eaf76ed505ba0ac71
Barys Dashko <b.dashko@godeltech.com>
2020-12-10 03:27:48 +0300

    Task 4 ready

---
+++ Tasks/Barys/Task4/query04.sql
@@ -0,0 +1,19 @@
+--Task 4
+WITH Orders_CTE 
+	(
+	SalesOrderID,
+	OrderDate,
+	CustomerID,
+	rownum
+	) AS
+	( SELECT 
+		SalesOrderID,
+		OrderDate,
+		CustomerID,
+		ROW_NUMBER() OVER(PARTITION BY OrderDate ORDER BY SalesOrderID) AS rownum
+	FROM Sales.SalesOrderHeader
+	)
+SELECT *
+FROM Orders_CTE
+WHERE rownum BETWEEN 11 AND 100
+;
