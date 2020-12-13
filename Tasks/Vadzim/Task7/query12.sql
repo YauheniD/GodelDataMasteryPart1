e7066181c8e6cd7d76d658527f733d12918f0567
Vadzim Rashkevich <v.rashkevich@godeltech.com>
2020-12-14 01:02:46 +0300

    Added query12

---
+++ Tasks/Vadzim/Task7/query12.sql
@@ -0,0 +1,11 @@
+﻿--Query 12
+UPDATE tempdb.dbo.Product
+SET ListPrice =  pp.ListPrice
+OUTPUT
+    INSERTED.ProductID AS Inserted,
+    DELETED.ProductID AS Deleted
+FROM tempdb.dbo.Product AS p
+JOIN AdventureWorks2019.Production.Product AS pp 
+    ON(p.ProductID = pp.ProductID)
+WHERE p.ModifiedDate != pp.ModifiedDate
+;
