955764a80a8a8bf1e1ce24d8c31845c946fd011f
Viachaslau Saroka <v.saroka@godeltech.com>
2020-12-14 01:20:49 +0300

    Task7

--- Tasks/Viachaslau/Task7/query08.sql
+++ Tasks/Viachaslau/Task7/query08.sql
@@ -3,21 +3,14 @@ USE tempdb;
 IF OBJECT_ID('dbo.Product') IS NOT NULL
        DROP TABLE dbo.Product;
 
-SELECT *
-INTO dbo.Product
-FROM AdventureWorks2019.Production.Product AS OH
-
 ALTER TABLE dbo.Product ADD RowID INT
 
 UPDATE T1
 SET T1.RowID = T1.NewRowNum
 FROM (
        SELECT RowID,
-              ROW_NUMBER() OVER (
-                     ORDER BY ProductID
-                     ) AS NewRowNum
-       FROM dbo.Product
-       ) T1
+              ROW_NUMBER() OVER (ORDER BY ProductID) AS NewRowNum
+       FROM dbo.Product) T1
 
 SELECT *
 FROM dbo.Product
