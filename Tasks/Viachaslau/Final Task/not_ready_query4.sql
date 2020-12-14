eaf23fabbf215f6a3ae9a35cd3c42e1a873ec0ef
Viachaslau Saroka <v.saroka@godeltech.com>
2020-12-14 10:49:34 +0300

    final task w/o query04

--- Tasks/Viachaslau/Final Task/not_ready_query4.sql
+++ Tasks/Viachaslau/Final Task/not_ready_query4.sql
@@ -1,13 +1,15 @@
 --something wrong with joins
-
-SELECT DISTINCT PROD_CAT.Name, SUM(ListPrice) OVER(PARTITION BY PROD_CAT.NAME) AS CategoryTotal 
-FROM Sales.SalesOrderDetail AS OD
-LEFT JOIN Production.Product AS PROD_PROD
-ON PROD_PROD.ProductID = OD.ProductID
-LEFT JOIN Production.ProductSubcategory AS PROD_SUBCAT
-ON PROD_SUBCAT.ProductCategoryID = PROD_PROD.ProductSubcategoryID
-LEFT JOIN Production.ProductCategory AS PROD_CAT
-ON PROD_SUBCAT.ProductCategoryID = PROD_CAT.ProductCategoryID
-/*LEFT JOIN Sales.SalesOrderHeader AS OH
-ON OH.SalesOrderID = OD.SalesOrderID
-WHERE OH.OrderDate BETWEEN '06/01/2014' AND '12/31/2014'*/
+SELECT DISTINCT PC.Name,
+       SUM(ListPrice) OVER (PARTITION BY PC.Name) AS CategoryTotal,
+       SUM(ListPrice) OVER (PARTITION BY P.Product) AS Total
+FROM Production.ProductCategory AS PC
+INNER JOIN Production.ProductSubcategory AS PS
+       ON PC.ProductCategoryID = PS.ProductCategoryID
+INNER JOIN Production.Product AS P
+       ON P.ProductSubcategoryID = PS.ProductSubcategoryID
+INNER JOIN Sales.SalesOrderDetail AS OD
+       ON OD.ProductID = P.ProductID
+INNER JOIN Sales.SalesOrderHeader AS OH
+       ON OH.SalesOrderID = OD.SalesOrderID
+WHERE OH.OrderDate BETWEEN '06/01/2014'
+              AND '12/31/2014'
