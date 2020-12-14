29a71b5826509e00b61fcbab6d4521ddb12739c4
Viachaslau Saroka <v.saroka@godeltech.com>
2020-12-14 11:06:32 +0300

    final task w/o query04

--- Tasks/Viachaslau/Final Task/query04.sql
+++ Tasks/Viachaslau/Final Task/query04.sql
@@ -1,9 +1,7 @@
---something wrong with joins
 SELECT DISTINCT PC.Name,
        SUM(ListPrice) OVER (PARTITION BY PC.Name) AS CategoryTotal,
-       SUM(ListPrice) OVER() AS OverallTotal,
-	   CAST(((CAST(SUM(ListPrice) OVER(PARTITION BY PC.Name) AS FLOAT) / (CAST(SUM(ListPrice) OVER() AS FLOAT)))) * 100 AS FLOAT) AS CategoryPercentage
-
+       SUM(ListPrice) OVER () AS OverallTotal,
+       CAST(((CAST(SUM(ListPrice) OVER (PARTITION BY PC.Name) AS FLOAT) / (CAST(SUM(ListPrice) OVER () AS FLOAT)))) * 100 AS FLOAT) AS CategoryPercentage
 FROM Production.ProductCategory AS PC
 INNER JOIN Production.ProductSubcategory AS PS
        ON PC.ProductCategoryID = PS.ProductCategoryID
@@ -15,3 +13,4 @@ INNER JOIN Sales.SalesOrderHeader AS OH
        ON OH.SalesOrderID = OD.SalesOrderID
 WHERE OH.OrderDate BETWEEN '06/01/2014'
               AND '12/31/2014'
+;
