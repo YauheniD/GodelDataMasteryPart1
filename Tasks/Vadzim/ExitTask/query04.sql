45f1fa39c825c24ef29b2ff9067b12945b1d0470
Vadzim Rashkevich <v.rashkevich@godeltech.com>
2020-12-14 10:57:10 +0300

    Fixed 4 query

--- Tasks/Vadzim/ExitTask/query04.sql
+++ Tasks/Vadzim/ExitTask/query04.sql
@@ -3,11 +3,9 @@ SELECT DISTINCT
     hrs.Name AS ShiftName,
     COUNT(*) OVER() AS OverallEmployees,
     COUNT(*) OVER(PARTITION BY hrs.Name) AS EmployeesByShift,
-    CAST(((CAST(COUNT(*) OVER(PARTITION BY hrs.Name) AS FLOAT) / (CAST(COUNT(*) OVER() AS FLOAT)))) * 100 AS INT) AS EmployeesByShiftPercentage
+    ROUND(((CAST(COUNT(*) OVER(PARTITION BY hrs.Name) AS FLOAT) / (CAST(COUNT(*) OVER() AS FLOAT)))) * 100 ,2) AS EmployeesByShiftPercentage
 FROM HumanResources.Shift AS hrs
-JOIN HumanResources.EmployeeDepartmentHistory AS hredh 
-    ON (hrs.ShiftID = hredh.ShiftID)
-JOIN HumanResources.Employee AS hre 
-    ON (hredh.BusinessEntityID = hre.BusinessEntityID)
+JOIN HumanResources.EmployeeDepartmentHistory AS hredh ON (hrs.ShiftID = hredh.ShiftID)
+JOIN HumanResources.Employee AS hre ON (hredh.BusinessEntityID = hre.BusinessEntityID)
 ORDER BY EmployeesByShiftPercentage DESC
 ;
